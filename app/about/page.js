418a3eba303a6ac9133101e7b05e5b194b2ead06
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-23 20:49:39 +0430

    page

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1 className="text-7xl">AboutPage</h1>
+            <Link href='/' className="text-2xl">home page</Link>
+        </div>
+    )
+}
+
+export default AboutPage
