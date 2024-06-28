4b6e9a954314ada6b4d69adf2bbf3fb963106272
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-28 08:24:22 +0430

    client page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Link from "next/link"
 
 const HomePage = () => {
+
   return (
     <div>
-      <h1 className="text-7xl">HomePage</h1>
-      <Link href='/about' className="text-2xl">about page</Link>
+      <h1 className="text-5xl mb-8 font-bold">HomePage</h1>
+      <Link href='/client' className="btn btn-accent">get started</Link>
+
     </div>
   )
 }
