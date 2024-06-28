3d425040175b29fcffa7690a24db14efcf973c27
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-28 10:56:52 +0430

    prisma

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,7 +1,30 @@
-const prismaExample = () => {
+import prisma from "@/utils/db"
+
+const prismaHandlers = async () => {
+    await prisma.task.create({
+        data: {
+            content: 'wake up'
+        },
+    })
+    const allTask = await prisma.task.findMany({
+        orderBy: {
+            createdAt: 'desc',
+        }
+    })
+    return allTask
+}
+
+const prismaExample = async () => {
+    const tasks = await prismaHandlers()
     return (
         <div>
-            <h1 className="text-7xl">prismaExample</h1>
+            <h1 className="text-7xl">
+                {tasks.map((tasks) => {
+                    return <h2 key={tasks.id} className="text-xl py-2">
+                        {tasks.content}
+                    </h2>
+                })}
+            </h1>
         </div>
     )
 }
