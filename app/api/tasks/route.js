a293054c5c297d7fb1f68136419047605c08ae1f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-29 18:43:10 +0430

    post request

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,6 +1,18 @@
 import db from '@/utils/db';
+import { NextResponse } from 'next/server';
 
 export const GET = async (request) => {
     const tasks = await db.task.findMany()
-    return Response.json({data:tasks})
+    return NextResponse.json({ data: tasks })
+}
+    
+export const POST = async (request) => {
+    const data = await request.json();
+    const task = await db.task.create({
+        data: {
+            content: data.content,
+
+        }
+    })
+    return Response.json({ msg: task })
 }
