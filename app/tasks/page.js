3efba1a66232a6c5dbfbd346d753d781d886a845
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-29 20:44:46 +0430

    dynamic

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,6 +1,7 @@
 import TaskForm from "@/component/TaskForm"
 import TaskFormCustom from "@/component/TaskFormCustom"
 import TaskList from "@/component/TaskList"
+export const dynamic = 'force-dynamic'
 
 const TaskPage = () => {
     return (
