9be80de5597f304102fffcd31f26e8432feefc0b
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-29 20:42:39 +0430

    tasks length

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -16,6 +16,9 @@ const prismaHandlers = async () => {
 
 const prismaExample = async () => {
     const tasks = await prismaHandlers()
+    if (tasks.length === 0) {
+        return <h2 className="mt-8 font-medium text-lg">No tasks to show..</h2>
+    }
     return (
         <div>
             <h1 className="text-7xl">
